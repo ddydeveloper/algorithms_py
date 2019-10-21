f8d29936437a019619eee96cf43bd133c3f38d65
Daniil <ddydeveloper@gmail.com>
2019-10-21 23:34:49 +0300

    Removed debug code

--- scr/capitalization_persents.py
+++ scr/capitalization_persents.py
@@ -9,7 +9,3 @@ def compound(amount, year_perсent, month_count):
 def compound_efficient(amount, year_perсent, month_count):
     month_perсent = year_perсent / 12
     return amount * (1 + month_perсent / 100) ** month_count
-
-
-print(compound(100000, 10, 12))
-print(compound_efficient(100000, 10, 12))
--- scr/linear_search.py
+++ scr/linear_search.py
@@ -7,11 +7,3 @@ def linear_search(array, value):
         return -1
     else:
         return result
-
-print(linear_search([1,2,3,4,5,6,3,3], 3))
-print(linear_search([6,1,2,3,4,6,5,6], 6))
-print(linear_search([1,2,3,4,5,6], 11))
-
-print(linear_search([1,6,15,41,20,14,2], 41))
-print(linear_search([1,6,15,41,20,14,2], 14))
-print(linear_search([1,6,15,41,20,14,2], 44))
