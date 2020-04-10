76d96c42b2a5c9eecf82c9d8c816d2e15e2008df
Daniil <ddydeveloper@gmail.com>
2020-04-10 23:58:44 +0300

    Update hash_table.py

--- src/algorithms/hash_table.py
+++ src/algorithms/hash_table.py
@@ -1,41 +1,92 @@
+# Structure to keep hash key value pair
+class HashItem:
+    def __init__(self, key, value):
+        self.key = key
+        self.value = value
+
+    def is_empty(self):
+        return self.key is None
+
+    def __str__(self):
+        return f"{self.key}:{self.value}"
+
+
 # Hash table implementation based on double hashing
 class HashTable:
     def __init__(self, size):
-        self._buckets = [None * size]
-        self._items_count = 0
         self._size = size
+        self._buckets = [HashItem(None, None) * self._size]
+
+        self._items_count = 0
         self._prime_number = 7
 
     def insert(self, key, value):
-        # check capacity
-        # check item already exists
-        # count hash
-        # check key for collision
-        # insert value by key
-        pass
+        if self._items_count / self._size < 0.7:
+            self.__resize(self._size * 2)
+
+        hash_item, item_idx = self.get(key)
+
+        if hash_item.key is not None:
+            raise ValueError(f"Key {key} is already exists in a hash table")
+
+        item_idx, = self.__get_free_idx(key)
+
+        if item_idx is None:
+            raise ValueError(f"Key {key} can't be added to a hash table")
+
+        self._buckets[item_idx] = HashItem(key, value)
 
     def delete(self, key):
-        # count hash
-        # check has_key
-        # remove item
-        # todo: probably re-hash collision items in order to fill first step item
-        pass
+        hash_item, item_idx = self.get(key)
+
+        if hash_item.key is None:
+            return
+
+        self._buckets[item_idx].key = None
+        self._buckets[item_idx].value = None
 
     def has_key(self, key):
-        # count hash
-        # look for key in loop
-        pass
+        return self.get(key).key is not None
 
     def get(self, key):
         # count hash
         # looks for value in loop
-        pass
+        return HashItem(1, 1), 1
 
-    def __count_hash(self, key, step):
-        (self.__init_hash(key) + step * self.__double_hash(key)) % self._size
+    def __resize(self, size):
+        self._size = size
+        self.__rehash()
 
-    def __init_hash(self, key):
+    def __rehash(self):
+        buckets = self._buckets
+
+        self._buckets = [HashItem(None, None) * self._size]
+        self._items_count = 0
+
+        for bucket in buckets:
+            if bucket.key is None:
+                continue
+
+            self.insert(bucket.key, bucket.value)
+
+    def __get_free_idx(self, key):
+        step = 1
+
+        while self._size > step:
+            item_idx = self.__get_hash(key, step)
+
+            if self._buckets[item_idx].is_empty():
+                return item_idx, step
+
+            step += 1
+
+        return None, step
+
+    def __get_hash(self, key, step):
+        return (self.__get_init_hash(key) + step * self.__get_double_hash(key)) % self._size
+
+    def __get_init_hash(self, key):
         return key % self._size
 
-    def __double_hash(self, key):
+    def __get_double_hash(self, key):
         return self._prime_number - (key % self._prime_number)
